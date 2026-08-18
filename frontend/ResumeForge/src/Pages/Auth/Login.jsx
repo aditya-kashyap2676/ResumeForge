fc5c5b0ed0ca6c4ebdc864da725549081c19d8c0
Aditya <..>
2026-08-18 18:05:24 +0530

    Changes

--- frontend/ResumeForge/src/Pages/Auth/Login.jsx
+++ frontend/ResumeForge/src/Pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {useNavigate} from "react-router-dom"
 import Input from '../../components/Inputs/Input'
+import { validateEmail } from '../../Utils/helper'
 const Login = () => {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
@@ -8,12 +9,30 @@ const Login = () => {
   const navigate = useNavigate()
 
   // Handle Login form Submit 
-  const handlelogin = async (e)=>{}
+    const handlelogin = async (e)=>{
+      e.preventDefault()
+      if(!validateEmail(email)){
+        setError("Please Enter a validate email address.")
+        return
+      }
+      if(!password){
+        setError("Please Enter the password.")
+        return
+      }
+      setError("")
+      //login api call
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
   return (
     <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
       <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
       <p className="text-xs text-slate-700 mt-[5px] mb-6">Please Enter your details to login </p>
       <form onSubmit={handlelogin}>
+        <div className="flex flex-col gap-4">
         <Input type="email" value={email} 
         onChange={({target})=>{setEmail(target.value)}}
         label="Email Address"
@@ -21,9 +40,11 @@ const Login = () => {
         <Input type="password" value={password} 
         onChange={({target})=>{setPassword(target.value)}}
         label="Password" 
-        placeholder='Minimum 8 characters'/>
+        placeholder='Minimum 8 characters'
+        />
+        </div>
         {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
-        <button type='submit' className="w-full bg-black text-white py-3 rounded-lg mt-5 ">LOGIN</button>
+        <button type='submit' className="w-full bg-black text-white py-3 rounded-lg mt-5 active:scale-95">LOGIN</button>
         <p className="text-[13px] text-slate-800 mt-3">Don't hava an account?{" "}
           <button className='font-medium text-primary underlined cursor-pointer'
           onClick={()=>{
--- frontend/ResumeForge/src/components/Inputs/Input.jsx
+++ frontend/ResumeForge/src/components/Inputs/Input.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6"
-
 const Input = ({ value, onChange, label, placeholder, type }) => {
     const [showpassword, setShowpassword] = useState(false)
     return (
-        <div>
+        <div className='flex flex-col gap-2'>
             <label className='text-[13px] text-slate-800'>{label}</label>
             <div className='input-box'>
                 <input type={type === "password" ? (showpassword ? "text" : "password") : type}
                     placeholder={placeholder}
-                    className="w-full bg-transparent outline-none"
+                    className="w-full bg-gray-100 p-2 rounded-lg"
                     value={value}
                     onChange={(e) => onChange(e)}
                 />
