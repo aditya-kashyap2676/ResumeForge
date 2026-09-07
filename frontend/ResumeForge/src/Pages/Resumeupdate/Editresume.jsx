ff79b10d1ab77f47c0355bfc983376918da8f7a6
Aditya <adityakashyap02676@gmail.com>
2026-09-07 18:14:30 +0530

    Contact Info Form

--- frontend/ResumeForge/src/Pages/Resumeupdate/Editresume.jsx
+++ frontend/ResumeForge/src/Pages/Resumeupdate/Editresume.jsx
@@ -17,6 +17,7 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apipaths";
 import Stepprogress from "../../components/Stepprogress";
 import ProfileInfoForm from "./Forms/ProfileInfoForm";
+import ContactInfoForm from "./Forms/ContactInfoForm";
 
 const EditResume = () => {
   const { resumeId } = useParams();
@@ -28,7 +29,7 @@ const EditResume = () => {
   const [baseWidth, setBaseWidth] = useState(800);
   const [openThemeSelector, setOpenThemeSelector] = useState(false);
   const [openPreviewModal, setOpenPreviewModal] = useState(false);
-  const [currentPage, setCurrentPage] = useState("profile-info");
+  const [currentPage, setCurrentPage] = useState("contact-info");
   const [progress, setProgress] = useState(0);
 
   const [resumeData, setResumeData] = useState({
@@ -133,6 +134,15 @@ const EditResume = () => {
           ]}
           onNext={validateAndNext}/>
         )
+        case "contact-info":
+          return(
+            <ContactInfoForm
+            contactInfo={resumeData?.contactInfo}
+            updateSection={(key,value)=>{
+              updateSection("contactInfo",key,value)
+            }}
+            />
+          )
         default:
           return null
     }
